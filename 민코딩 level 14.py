e591cf0a449336027c6068a8afeff448ced25e95
Your Name <you@example.com>
2022-08-19 02:18:57 +0900

    0818

---
+++ 민코딩 level 14.py
@@ -0,0 +1,19 @@
+#1
+# a,b = map(int,input().split())
+#
+# while a > b :
+#     print(a,end=' ')
+#     a = a+1
+
+#-------------------------------------------------------
+
+#2
+arr = [list(map(int,input().split())) for _ in range(5)]
+sum =[0]*5
+
+for i in range(5):
+    sum.append(arr[])
+    for j in range(4):
+
+
+        arr[i][j]
